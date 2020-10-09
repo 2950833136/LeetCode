f120bd6ee6e5d3cdb4d13a75401e137dd687ba50
2950833136 <45315371+2950833136@users.noreply.github.com>
2020-10-10 00:20:22 +0800

    Update 39组合总和.cpp

--- 20-39/39组合总和.cpp
+++ 20-39/39组合总和.cpp
@@ -5,11 +5,11 @@
 /**
  * 算法思想：DFS + 回溯
  *  1、初始化参数
- *      group:  表示第几组路径
- *      result: 保存每条路径
- *      path:   用于保存路径
- *      num:    表示当前路径中的元素个数
- *      level:  防止选择之前的数
+ *      returnSize:     表示第几组路径
+ *      ret:            保存每条路径
+ *      eachItem:       用于保存单条路径
+ *      eachItemIndex:  表示当前路径中的元素个数
+ *      level:          防止选择之前的数，当前数下标
  *  2、DFS进行遍历时
  *      先对 target 进行判断
  *      若 target > 0 时，将所有满足 target - candidates[i] >= 0 的元素加入，继续执行DFS
@@ -18,37 +18,35 @@
  */
 #define MAX_RET 1000
 
-void dfs(int* candidates, int candidatesSize, int target, int* group, int** returnColumnSizes, int** result, int* path, int num, int level) {
-    if (target == 0) {
-        result[*group] = (int*)malloc(sizeof(int) * num);
-        for (int i = 0; i < num; i++) {
-            result[*group][i] = path[i];
-        }
-        (*returnColumnSizes)[*group] = num;
-        (*group)++;
+void dfs(int* candidates, int candidatesSize, int target, int* returnSize, int* columnSizes, int** ret, int* eachItem, int eachItemIndex, int level) {
+    if (target == 0 && eachItemIndex != 0) {
+        ret[*returnSize] = (int*)calloc(eachItemIndex, sizeof(int));
+        memcpy(ret[*returnSize], eachItem, eachItemIndex * sizeof(int));
+        columnSizes[*returnSize] = eachItemIndex;
+        (*returnSize)++;
         return;
     }
     for (int i = level; i < candidatesSize; i++) {
         if (target < 0) {
             return;
         }
-        path[num] = candidates[i];
-        dfs(candidates, candidatesSize, target - candidates[i], group, returnColumnSizes, result, path, num + 1, i);
+        eachItem[eachItemIndex] = candidates[i];
+        dfs(candidates, candidatesSize, target - candidates[i], returnSize, columnSizes, ret, eachItem, eachItemIndex + 1, i);
     }
 }
 
 int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes) {
-    if (target == 0) {
-        *returnSize = 0;
+    *returnSize = 0;
+    if (candidates == NULL || candidatesSize <= 0) {
         return NULL;
     }
-    *returnColumnSizes = (int*)malloc(sizeof(int) * MAX_RET);
-    int   group        = 0;
-    int** result       = (int**)malloc(sizeof(int*) * MAX_RET);
-    int*  path         = (int*)malloc(sizeof(int) * MAX_RET);
-    dfs(candidates, candidatesSize, target, &group, returnColumnSizes, result, path, 0, 0);
-    *returnSize = group;
-    return result;
+    int** ret           = (int**)calloc(MAX_RET, sizeof(int*));
+    int*  columnSizes   = (int*)calloc(MAX_RET, sizeof(int));
+    int*  eachItem      = (int*)calloc(MAX_RET, sizeof(int));
+    int   eachItemIndex = 0;
+    dfs(candidates, candidatesSize, target, returnSize, columnSizes, ret, eachItem, eachItemIndex, 0);
+    *returnColumnSizes = columnSizes;
+    return ret;
 }
 
 int main() {
