6a45ec4dcef91774b0101c62085068cf89d6364f
2950833136 <2950833136@qq.com>
2020-10-13 12:35:06 +0800

    add

---
+++ 480-499/495提莫攻击.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * 数组遍历
+ *      - 如果小于持续时间，加上间隔时间
+ *      - 否则，加上持续时间
+ */
+int findPoisonedDuration(int* timeSeries, int timeSeriesSize, int duration) {
+    int ans = 0;
+    if (timeSeriesSize == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < timeSeriesSize - 1; i++) {
+        if (timeSeries[i] + duration > timeSeries[i + 1]) {
+            ans += timeSeries[i + 1] - timeSeries[i];
+        } else {
+            ans += duration;
+        }
+    }
+    ans += duration;
+    return ans;
+}
+
+int main() {
+    int timeSeries[]   = {1, 2, 3, 4, 5};
+    int timeSeriesSize = sizeof(timeSeries) / sizeof(timeSeries[0]);
+    int duration       = 5;
+
+    int ans = findPoisonedDuration(timeSeries, timeSeriesSize, duration);
+    printf("%d", ans);
+
+    return 0;
+}
