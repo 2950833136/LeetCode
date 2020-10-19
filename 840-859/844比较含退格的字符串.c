4407984ee34c9829eaef4823ff55120278a724fb
2950833136 <2950833136@qq.com>
2020-10-19 09:20:25 +0800

    2020/10/19

--- 840-859/844比较含退格的字符串.c
+++ 840-859/844比较含退格的字符串.c
@@ -19,25 +19,79 @@ void display(char* str) {
  * 栈思想
  * 字母压入， '#' 压出
  */
-char* update(char* str) {
-    int   index = 0;
-    int   n     = strlen(str);
-    char* temp  = (char*)malloc(sizeof(char) * (n + 1));
+// char* update(char* str) {
+//     int   index = 0;
+//     int   n     = strlen(str);
+//     char* temp  = (char*)malloc(sizeof(char) * (n + 1));
 
-    for (int i = 0; i < n; i++) {
-        if (str[i] != '#') {
-            temp[index++] = str[i];
-        } else if (index > 0) {
-            index--;
-        }
-    }
-    temp[index] = '\0';
-    // display(temp);
-    return temp;
-}
+//     for (int i = 0; i < n; i++) {
+//         if (str[i] != '#') {
+//             temp[index++] = str[i];
+//         } else if (index > 0) {
+//             index--;
+//         }
+//     }
+//     temp[index] = '\0';
+//     // display(temp);
+//     return temp;
+// }
 
+// bool backspaceCompare(char* S, char* T) {
+//     return strcmp(update(S), update(T)) == 0;
+// }
+
+/**
+ * 双指针
+ *  skip 记录 '#' 的个数
+ *  从后往前遍历
+ */
 bool backspaceCompare(char* S, char* T) {
-    return strcmp(update(S), update(T)) == 0;
+    int i     = strlen(S) - 1;
+    int j     = strlen(T) - 1;
+    int skipS = 0;
+    int skipT = 0;
+
+    while (i >= 0 || j >= 0) {
+        // 逆序找到 S 没被删除的数
+        while (i >= 0) {
+            if (S[i] == '#') {
+                skipS++, i--;
+            } else if (skipS > 0) {
+                skipS--, i--;
+            } else {
+                break;
+            }
+        }
+        // 逆序找到 T 没被删除的数
+        while (j >= 0) {
+            if (T[j] == '#') {
+                skipT++, j--;
+            } else if (skipT > 0) {
+                skipT--, j--;
+            } else {
+                break;
+            }
+        }
+        /**
+         * 判断是否相等
+         *  + 都还有字符
+         *      - 不相等返回 false
+         *  + 其中一个没有字符，另一个还有
+         *      - 返回 false
+         */
+        if (i >= 0 && j >= 0) {
+            if (S[i] != T[j]) {
+                return false;
+            }
+        } else {
+            if (i >= 0 || j >= 0) {
+                return false;
+            }
+        }
+        // 相等继续循环
+        i--, j--;
+    }
+    return true;
 }
 
 int main() {
